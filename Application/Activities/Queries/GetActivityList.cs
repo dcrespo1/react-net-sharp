0589af2ba0f5d87c596f3ddaf4f3bff7802c1ea9
dcrespo1 <88146344+dcrespo1@users.noreply.github.com>
2025-06-16 10:28:29 -0400

    Added DELETE method, basic CRUD complete (#5)
    Co-authored-by: Dylan Crespo <dylancrespo@cloudfitsoftware.com>

--- Application/Activities/Queries/GetActivityList.cs
+++ Application/Activities/Queries/GetActivityList.cs
@@ -2,6 +2,7 @@ using System;
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Persistence;
 
 namespace Application.Activities.Queries;
@@ -14,6 +15,21 @@ public class GetActivityList
     {
         public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
         {
+            //This is an example of how to use the cancellation token
+            // try
+            // {
+            //     for (int i = 0; i < 10; i++)
+            //     {
+            //         cancellationToken.ThrowIfCancellationRequested();
+            //         await Task.Delay(1000, cancellationToken);
+            //         logger.LogInformation($"Task {i} has completed");
+            //     }
+            // }
+            // catch (System.Exception)
+            // {
+
+            //     logger.LogInformation("task was canceled");
+            // }
             return await context.Activities.ToListAsync(cancellationToken);
         }
     }
